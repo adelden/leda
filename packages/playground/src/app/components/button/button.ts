0bf8afcb91a18d0ecf0bf8964481321469652b89
ADenert <adenert.pro@gmail.com>
2025-12-24 23:25:36 +0100

    feat(buttons): create some variants

--- packages/playground/src/app/components/button/button.ts
+++ packages/playground/src/app/components/button/button.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger';
+export type ButtonSize = 'sm' | 'md' | 'lg';
 
 @Component({
   selector: 'app-button',
@@ -6,4 +9,17 @@ import { Component } from '@angular/core';
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
-export class Button {}
+export class Button {
+  readonly type = input<ButtonVariant>('primary');
+  readonly size = input<ButtonSize>('md');
+  readonly disabled = input<boolean>(false);
+  readonly loading = input<boolean>(false);
+
+  readonly clicked = output<MouseEvent>();
+
+  onClick(event: MouseEvent): void {
+    if (!this.disabled && !this.loading) {
+      this.clicked.emit(event);
+    }
+  }
+}
