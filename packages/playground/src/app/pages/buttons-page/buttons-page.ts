108d87509d93c491fd51db3b9426d8260cfccf18
ADenert <adenert.pro@gmail.com>
2025-12-24 22:49:25 +0100

    init archi in playgroung

---
+++ packages/playground/src/app/pages/buttons-page/buttons-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Button } from '../../components/button/button';
+
+@Component({
+  selector: 'app-buttons-page',
+  imports: [Button],
+  templateUrl: './buttons-page.html',
+  styleUrl: './buttons-page.scss',
+})
+export class ButtonsPage {}
